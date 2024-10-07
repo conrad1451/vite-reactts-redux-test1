ecf6a1a983ba2b3a5904f7b3a7b973caf14efdf9
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-10-07 04:54:42 -0400

    fix: failure to implement redux in vite ReactTS app

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import store from './redux/store';
+import { Provider } from 'react-redux';
+// Cannot find module './redux' or its corresponding type declarations.ts(2307)
+// import { Provider } from './redux';
+
+import AppRedux from './AppRedux.tsx';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+			<AppRedux />
+		</Provider>
+    {/* <App /> */}
   </StrictMode>,
 )
